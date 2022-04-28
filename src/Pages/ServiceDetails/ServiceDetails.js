8fd83317673de1cbcf844698c20b431f8addcd3b
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-04-29 02:48:17 +0600

    implemented loading a single service

--- src/Pages/ServiceDetails/ServiceDetails.js
+++ src/Pages/ServiceDetails/ServiceDetails.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 
 const ServiceDetails = () => {
     const { Id } = useParams();
+    const url = `http://localhost:5000/service/${Id}`
+    const [service, setService] = useState({});
+    useEffect(() => {
+        fetch(url)
+            .then(res => res.json())
+            .then(data => setService(data))
+    }, []);
     return (
         <div>
-            <h2>Service Details of: {Id}</h2>
+            <h2>Service Details of: {service.name}</h2>
             <Link to='/checkout'>
                 <button className='btn btn-dark'>Proceed to checkout</button>
             </Link>
