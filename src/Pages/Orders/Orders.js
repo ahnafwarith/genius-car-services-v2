394ccfe12744cc8e72b27ded6904b81ea3c81ab5
ahnafwarith <corp.ahnafwarid@gmail.com>
2022-05-02 05:49:46 +0600

    displaying orders of user individually

--- src/Pages/Orders/Orders.js
+++ src/Pages/Orders/Orders.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect, useState } from 'react';
+import { useAuthState } from 'react-firebase-hooks/auth';
+import auth from '../../firebase.init';
 
 const Orders = () => {
+    const [user] = useAuthState(auth);
+    const [orders, setOrders] = useState([]);
+    useEffect(() => {
+        const getOrders = async () => {
+            const email = user.email
+            const url = `http://localhost:5000/order?email=${email}`;
+            const { data } = await axios.get(url);
+            setOrders(data);
+        }
+        getOrders();
+
+    }, [user]);
     return (
         <div>
-            <h1>This is orders</h1>
+            <h1>{orders.length}</h1>
         </div>
     );
 };
